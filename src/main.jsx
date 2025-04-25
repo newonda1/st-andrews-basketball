bc1a06b608ef6e3b4c659c0e34df27cdb9bffc7b
newonda1 <nicholasbroom@gmail.com>
2025-04-25 15:15:46 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Season2024 from "./pages/Season2024_2025.jsx";
+import Season2024 from "./pages/Season2024.jsx";
 import GamePage from "./pages/GamePage.jsx";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
