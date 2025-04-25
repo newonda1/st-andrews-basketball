d3174ee2a363b3c2c91b92e44e009539bae49e1b
newonda1 <nicholasbroom@gmail.com>
2025-04-25 13:33:26 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Season2024_2025 from "./pages/Season2024_2025.jsx";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/season/2024-2025" element={<Season2024_2025 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
