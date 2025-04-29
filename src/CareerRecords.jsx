590a9e5fa8c875fa7ccadc1a64f811656df8739c
newonda1 <nicholasbroom@gmail.com>
2025-04-28 20:55:24 -0400

    Update CareerRecords.jsx

--- src/CareerRecords.jsx
+++ src/CareerRecords.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+function CareerRecords() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Career Records</h1>
+      <p>This page will display all-time career records.</p>
+    </div>
+  );
+}
+
+export default CareerRecords;
