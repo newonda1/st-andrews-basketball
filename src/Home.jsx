7c0d68c5a34f0effc2348aaeedf7e3e5135d3142
newonda1 <nicholasbroom@gmail.com>
2025-04-28 21:04:47 -0400

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <section className="mb-12">
+      <div className="bg-gray-100 rounded-xl p-8 shadow-md">
+        {/* Copy ALL your existing St. Andrew's history content here */}
+      </div>
+    </section>
+  );
+}
+
+export default Home;
