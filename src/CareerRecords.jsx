ba6202e41c81ef6ee15fe62234ee704d51790743
newonda1 <nicholasbroom@gmail.com>
2025-04-29 06:32:52 -0400

    Update CareerRecords.jsx

--- src/CareerRecords.jsx
+++ src/CareerRecords.jsx
@@ -97,13 +97,10 @@ function CareerRecords() {
             <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('GamesPlayed')}>Games Played</th>
             <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('3PM')}>3PM</th>
             <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('3PA')}>3PA</th>
-            <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('3P%')}>3P%</th>
             <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('2PM')}>2PM</th>
             <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('2PA')}>2PA</th>
-            <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('2P%')}>2P%</th>
             <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('FTM')}>FTM</th>
             <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('FTA')}>FTA</th>
-            <th className="px-4 py-2 cursor-pointer" onClick={() => handleSort('FT%')}>FT%</th>
             
           </tr>
         </thead>
