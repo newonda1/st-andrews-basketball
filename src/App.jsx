5e4a97ff3e82418e4f152c88d0819c46dd95532a
newonda1 <nicholasbroom@gmail.com>
2025-05-10 12:09:30 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -241,7 +241,7 @@ function App() {
         <Route path="/records/season" element={<SeasonRecords />} />
         <Route path="/records/single-game" element={<SingleGameRecords />} />
         <Route path="/legacy/ChrisHaslam" element={<ChrisHaslam />} />
-        <Route path="/seasons/2024-25" element={<Season2024_25 />} />
+        <Route path="/seasons/:seasonId" element={<SeasonPlaceholder />} />
       </Routes>
   </div>
 );
