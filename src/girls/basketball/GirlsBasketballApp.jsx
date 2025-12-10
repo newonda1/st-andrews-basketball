422cab45fe090adb4074a8805394f9122b6a4103
newonda1 <nicholasbroom@gmail.com>
2025-12-10 10:59:44 -0500

    Update GirlsBasketballApp.jsx

--- src/girls/basketball/GirlsBasketballApp.jsx
+++ src/girls/basketball/GirlsBasketballApp.jsx
@@ -1,19 +1,12 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Season2025_26 from "./seasons/Season2025_26";
-import PlayerPage from "./pages/PlayerPage";
-import GameDetail from "./pages/GameDetail";
 
 function GirlsBasketballApp() {
   return (
     <Routes>
-      {/* Landing page for the girls site */}
+      {/* Landing page for girls basketball */}
       <Route path="/" element={<Season2025_26 />} />
-
-      {/* Individual game pages */}
-      <Route path="/games/:gameId" element={<GameDetail />} />
-
-      {/* Individual player pages */}
-      <Route path="/players/:playerId" element={<PlayerPage />} />
     </Routes>
   );
 }
