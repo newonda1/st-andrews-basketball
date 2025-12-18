4fc4d64bdae029f9c1ecd42f77f0e158125e753d
newonda1 <nicholasbroom@gmail.com>
2025-12-18 08:57:38 -0500

    Update BoysBasketballApp.jsx

--- src/boys/basketball/BoysBasketballApp.jsx
+++ src/boys/basketball/BoysBasketballApp.jsx
@@ -6,6 +6,7 @@ import FullCareerStats from "./pages/FullCareerStats";
 import SeasonRecords from "./pages/SeasonRecords";
 import SingleGameRecords from "./pages/SingleGameRecords";
 
+import Season1978_93 from "./seasons/Season1978_79";
 import Season1992_93 from "./seasons/Season1992_93";
 import Season2023_24 from "./seasons/Season2023_24";
 import Season2024_25 from "./seasons/Season2024_25";
@@ -18,6 +19,7 @@ import GameDetail from "./pages/GameDetail";
 import PlayerPage from "./pages/PlayerPage";
 
 const seasonPages = [
+  { slug: "1978-79", Component: Season1978_79 },
   { slug: "1992-93", Component: Season1992_93 },
   { slug: "2023-24", Component: Season2023_24 },
   { slug: "2024-25", Component: Season2024_25 },
