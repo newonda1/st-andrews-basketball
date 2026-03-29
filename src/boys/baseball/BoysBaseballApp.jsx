e3b045f80819e2a0ddb62877eb656ebd39570db2
newonda1 <nicholasbroom@gmail.com>
2026-03-29 19:30:01 -0400

    Update BoysBaseballApp.jsx

--- src/boys/baseball/BoysBaseballApp.jsx
+++ src/boys/baseball/BoysBaseballApp.jsx
@@ -4,6 +4,7 @@ import AthleticsProgramShell from "../../components/AthleticsProgramShell";
 
 import YearlyResults from "./pages/YearlyResults";
 import Season2026 from "./seasons/Season2026";
+import Season2025 from "./seasons/Season2025";
 import GameDetail from "./pages/GameDetail";
 import PlayerPage from "./pages/PlayerPage";
 
@@ -40,6 +41,10 @@ export default function BoysBaseballApp() {
             to: "/athletics/boys/baseball/seasons/2026",
             label: "2026 Season",
           },
+          {
+            to: "/athletics/boys/baseball/seasons/2025",
+            label: "2025 Season",
+          },
         ],
       },
     ],
@@ -60,6 +65,7 @@ export default function BoysBaseballApp() {
       <Routes>
         <Route index element={<YearlyResults />} />
         <Route path="seasons/2026" element={<Season2026 />} />
+        <Route path="seasons/2025" element={<Season2025 />} />
         <Route path="games/:gameId" element={<GameDetail />} />
         <Route path="players/:playerId" element={<PlayerPage />} />
 
