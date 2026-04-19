c4e9a178b2d3995058f9b487055d1c66a26c0d5c
newonda1 <nicholasbroom@gmail.com>
2026-04-19 09:31:09 -0400

    Add 1979-80 season page

--- src/boys/basketball/BoysBasketballApp.jsx
+++ src/boys/basketball/BoysBasketballApp.jsx
@@ -18,6 +18,7 @@ import GameDetailHistorical from "./pages/GameDetailHistorical";
 import PlayerPage from "./pages/PlayerPage";
 
 import Season1978_79 from "./seasons/Season1978_79";
+import Season1979_80 from "./seasons/Season1979_80";
 import Season1992_93 from "./seasons/Season1992_93";
 import Season2022_23 from "./seasons/Season2022_23";
 import Season2023_24 from "./seasons/Season2023_24";
@@ -27,6 +28,7 @@ import SeasonPlaceholder from "./seasons/SeasonPlaceholder";
 
 const seasonPages = [
   { slug: "1978-79", Component: Season1978_79 },
+  { slug: "1979-80", Component: Season1979_80 },
   { slug: "1992-93", Component: Season1992_93 },
   { slug: "2022-23", Component: Season2022_23 },
   { slug: "2023-24", Component: Season2023_24 },
