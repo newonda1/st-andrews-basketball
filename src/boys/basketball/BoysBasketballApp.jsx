831eb49dbeafa68879e70f541191ab18db37fefd
newonda1 <nicholasbroom@gmail.com>
2026-04-19 20:47:39 -0400

    Add 2008-09 stats and season page

--- src/boys/basketball/BoysBasketballApp.jsx
+++ src/boys/basketball/BoysBasketballApp.jsx
@@ -38,6 +38,7 @@ import Season1995_96 from "./seasons/Season1995_96";
 import Season2000_01 from "./seasons/Season2000_01";
 import Season2001_02 from "./seasons/Season2001_02";
 import Season2002_03 from "./seasons/Season2002_03";
+import Season2008_09 from "./seasons/Season2008_09";
 import Season2022_23 from "./seasons/Season2022_23";
 import Season2023_24 from "./seasons/Season2023_24";
 import Season2024_25 from "./seasons/Season2024_25";
@@ -66,6 +67,7 @@ const seasonPages = [
   { slug: "2000-01", Component: Season2000_01 },
   { slug: "2001-02", Component: Season2001_02 },
   { slug: "2002-03", Component: Season2002_03 },
+  { slug: "2008-09", Component: Season2008_09 },
   { slug: "2022-23", Component: Season2022_23 },
   { slug: "2023-24", Component: Season2023_24 },
   { slug: "2024-25", Component: Season2024_25 },
